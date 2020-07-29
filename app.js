a7804cfcd305bd68b2960f96dbd554c6e8013e44
Franklin Isalguez <franklingonzalez@neoscloudllc.com>
2020-07-29 16:13:03 -0400

    Add reponse status and custom header

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/demo", (req, res) => {
+  res.status(418);
+  res.set("X-Franklin", "Test Franklin");
+  res.send("I'm a teapot demo");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
